52da913a23c26a2bdf32eb82d7ba872277671c46
Lizzy Zhou <zhongle@unc.edu>
2023-07-27 15:04:56 -0400

    Updated the solutionStackName

--- lib/eb-appln-stack.ts
+++ lib/eb-appln-stack.ts
@@ -85,7 +85,7 @@ export class EBApplnStack extends cdk.Stack {
      const elbEnv = new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
        environmentName: props?.envName ?? "MyWebAppEnvironment",
        applicationName: app.applicationName || appName,
-       solutionStackName: '64bit Amazon Linux 2 v5.8.0 running Node.js 18',
+       solutionStackName: '64bit Amazon Linux 2 v5.8.3 running Node.js 18',
        optionSettings: optionSettingProperties,
        versionLabel: appVersionProps.ref,
      });
