7de67bc5a46ade717fca403cc78a3bc12cdff550
Lizzy Zhou <zhongle@unc.edu>
2023-07-27 13:17:57 -0400

    Add Pre-Prod stage

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,6 +1,8 @@
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { CdkEBStage } from './eb-stage';
+
 
 export class CdkPipelineStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -14,5 +16,18 @@ export class CdkPipelineStack extends Stack {
                 commands: ['npm ci', 'npm run build', 'npx cdk synth'],
             }),
         });
+
+        // This is where we add the application stages
+        // deploy beanstalk app
+        // For environment with all default values:
+        // const deploy = new CdkEBStage(this, 'Pre-Prod');
+
+        // For environment with custom AutoScaling group configuration
+        const deploy = new CdkEBStage(this, 'Pre-Prod', {
+            minSize: "1",
+            maxSize: "2"
+        });
+        const deployStage = pipeline.addStage(deploy); 
+        
     }
 }
